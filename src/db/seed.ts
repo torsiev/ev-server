da8c6d2c28ad361ac2d3772c906b88efa2c8c3e7
Hermanus Hasta Wicaksana <hasta.wicaksana@gmail.com>
2024-04-25 10:55:23 +0700

    feat: Create seed for chargeboxes

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,6 +1,7 @@
 import { eq } from 'drizzle-orm';
 import { MySql2Database } from 'drizzle-orm/mysql2';
-import { addresses, ocppTags, users } from './schema';
+import { addresses, chargeboxes, connectors, ocppTags, users } from './schema';
+import { WssProtocol } from 'types/server';
 
 export default async function seed(db: MySql2Database) {
   // Insert admin data to users table
@@ -19,13 +20,36 @@ export default async function seed(db: MySql2Database) {
 
   // Insert admin adddress
   const addressesTable = await db.insert(addresses).values({
-    street: 'Jl Telekomunikasi 1',
+    street: 'Jl Telekomunikasi 1, Gedung Selaru',
     zipCode: '40257',
     city: 'Kabupaten Bandung',
     country: 'Indonesia',
   });
 
-  // Update user data with ocpp tag id and address id
+  // Insert addresses chargeboxes
+  const chargeBoxesAddress = await db.insert(addresses).values({
+    street: 'Jl Telekomunikasi 1, Gedung Telkom University Landmark Tower',
+    zipCode: '40257',
+    city: 'Kabupaten Bandung',
+    country: 'Indonesia',
+  });
+
+  // Insert data to chargeboxes
+  const chargeBoxesTable = await db.insert(chargeboxes).values({
+    addressId: chargeBoxesAddress[0].insertId,
+    identifier: '123',
+    ocppProtocol: WssProtocol.OCPP16,
+    locationLatitude: '38.8958',
+    locationLongitude: '-132.4095',
+  });
+
+  // Insert data to connectorsTable
+  await db.insert(connectors).values({
+    chargeboxId: chargeBoxesTable[0].insertId,
+    connectorId: chargeBoxesTable[0].insertId,
+  });
+
+  //Update user data with ocpp tag id and address id
   await db
     .update(users)
     .set({
